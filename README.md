315e7a9c84859755f84503520d60d589b134473d
zeel-codder <zeelprajapati321@gmail.com>
2024-01-20 20:57:17 +0530

    feat: Update ReadMe File

--- README.md
+++ README.md
@@ -2,13 +2,32 @@
 
 Jetha-Bhai CLI is a command-line interface that provides a fun and unique way to interact with your file system. It mimics some common shell commands, but with a twist. Each command is translated into the language of Jetha-Bhai, a popular character from Indian television.
 
+![Alt text](./doc/output.png)
+
 ## Installation
 
-## Cmd Map
+1. Install the latest release (v0.1.0)
 
-| Command     | Translation                     |
+2. Install the Python package with pip
+
+```bash
+pip install jetha_cli-0.1.0.tar.gz
+or
+pip install jetha_cli-0.1.0-py3-none-any.whl
+```
+
+# Test Cmd
+
+```bash
+jetha-bhai --help
+jetha-bhai ye-banado f1
+jetha-bhai list-kardo
+```
+
+## Table
+
+| command     | jetha-cli                       |
 | ----------- | ------------------------------- |
-| cmd         | jetha-bhai mod                  |
 | pwd         | jetha-bhai babita-ji-kaha       |
 | ls          | jetha-bhai list-kardo           |
 | mkdir c1    | jetha-bdhai ye-banado c1        |
@@ -16,4 +35,4 @@ Jetha-Bhai CLI is a command-line interface that provides a fun and unique way to
 | rm -rf f1   | jetha-bhai udado-isko f1        |
 | cp -r f1 f2 | jetha-bhai copy-maro f1 f2      |
 
-# Test Cmd
+# Thank you
--- jetha_cli/src/__init__.py
+++ jetha_cli/src/__init__.py
@@ -1,7 +1,16 @@
 import os
+import random
 import shutil
 
 import click
+from rich import print
+
+
+def print_rainbow(line):
+    colors = ["orange", "yellow", "green", "blue", "indigo", "violet"]
+    color = random.choice(colors)
+    print("")
+    print(f"[{color}]{line}[/{color}]")
 
 
 @click.group("jetha-bhai")
@@ -11,21 +20,21 @@ def jetha_bhai():
 
 @jetha_bhai.command("babita-ji-kaha", help="Display the current working directory")
 def pwd():
-    print(f"Babita ji, yeh current working directory mai hai: {os.getcwd()}")
+    print_rainbow(f"Babita ji, yeh current working directory mai hai: {os.getcwd()}")
 
 
 @jetha_bhai.command(
     "list-kardo", help="List files and directories in the current folder"
 )
 def ls():
-    print("Bhai, yeh le list, jalebi bhej dena par yaad se")
-    print("\n".join(os.listdir(os.getcwd())))
+    print_rainbow("Bhai, yeh le list, jalebi bhej dena par yaad se")
+    print_rainbow("\n".join(os.listdir(os.getcwd())))
 
 
 @jetha_bhai.command("ye-banado", help="Create a new directory")
 @click.argument("folder")
 def mkdir(folder):
-    print("Mota bhai kaam kar diya hai, naya folder ban gaya")
+    print_rainbow("Mota bhai kaam kar diya hai, naya folder ban gaya")
     os.makedirs(folder, exist_ok=True)
 
 
@@ -33,14 +42,14 @@ def mkdir(folder):
 @click.argument("src_folder")
 @click.argument("destination_folder")
 def mv(src_folder, destination_folder):
-    print("Yei vayadi move on nahi move hota hai voh, maine kar diya kam kher")
+    print_rainbow("Yei vayadi move on nahi move hota hai voh, maine kar diya kam kher")
     shutil.move(src_folder, destination_folder)
 
 
 @jetha_bhai.command("udado-isko", help="Remove a folder")
 @click.argument("folder")
 def rm(folder):
-    print("Sunder ko to uda nahi sakta, lekin folder ko uda diya maine")
+    print_rainbow("Sunder ko to uda nahi sakta, lekin folder ko uda diya maine")
     shutil.rmtree(folder)
 
 
@@ -48,7 +57,9 @@ def rm(folder):
 @click.argument("src_folder")
 @click.argument("destination_folder")
 def cp(src_folder, destination_folder):
-    print("Tapu ki sangat ka asar laga hai tume sayad, copying kar raha hoon mai chalo")
+    print_rainbow(
+        "Tapu ki sangat ka asar laga hai tume sayad, copying kar raha hoon mai chalo"
+    )
     shutil.copytree(src_folder, destination_folder)
 
 
